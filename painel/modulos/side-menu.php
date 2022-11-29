b0ce804eefec54203356fcb395f5514d1e5f3d5d
Trevikkj <guitrevisan2501@gmail.com>
2022-11-29 20:32:37 -0300

    CREATED @ Carregando arquivo iniciais

---
+++ painel/modulos/side-menu.php
@@ -0,0 +1,36 @@
+<div id="side-menu">
+
+    <div id="menu-block">
+        VAPOR
+    </div>
+
+    <a href="jogos.php" title="Jogos">
+        <div class="item">
+            <span>Jogos</span>
+        </div>
+    </a>
+    <a href="plataformas.php" title="Plataformas">
+        <div class="item">
+            <span>Plataformas</span>
+        </div>
+    </a>
+
+    <a href="idiomas.php" title="Plataformas">
+        <div class="item">
+            <span>Idiomas</span>
+        </div>
+    </a>
+
+    <a href="categorias.php" title="Categorias">
+        <div class="item">
+            <span>Categorias</span>
+        </div>
+    </a>
+
+    <a href="logout.php" title="Sair">
+        <div class="item">
+            <span>Sair</span>
+        </div>
+    </a>
+
+</div>
